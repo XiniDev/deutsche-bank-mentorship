86c46f838b3b12fd61ab8506ca409be5916aaaf0
Anthony Leung <anthonyleung07@gmail.com>
2022-02-20 01:06:44 +0000

    login and register change
    change

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -5,28 +5,30 @@ import Cat from '../images/Cat.svg';
 const Login = () => {
     return (
         <div className="frontpage">
-            <div className="frontpage__title">
-                Want to get more work on top of your job?
-            </div>
-            <div className="frontpage__subtitle">
-                Use our software.
-            </div>
-            <img src={Cat} className="frontpage__img"/>
-            <div className="login__form__area">
-                <div className="login__form__title">
-                    <hr/>
-                    Sign In
-                    <hr/>
+            <div className="frontpage__block">
+                <div className="frontpage__title">
+                    Want to get more work on top of your job?
                 </div>
-                <form className="login__form">
-                    <label>
-                        <input type="text" name="email" placeholder="Email Address"/><br/>
-                    </label>
-                    <label>
-                        <input type="password" name="password" placeholder="Password"/><br/>
-                    </label>
-                    <input type="submit" className="login__form__submit" value="Sign In"/>
-                </form>
+                <div className="frontpage__subtitle">
+                    Use our software.
+                </div>
+                <img src={Cat} className="frontpage__img"/>
+                <div className="login__form__area">
+                    <div className="login__form__title">
+                        <hr/>
+                        Sign In
+                        <hr/>
+                    </div>
+                    <form className="login__form">
+                        <label>
+                            <input type="text" name="email" placeholder="Email Address"/><br/>
+                        </label>
+                        <label>
+                            <input type="password" name="password" placeholder="Password"/><br/>
+                        </label>
+                        <input type="submit" className="login__form__submit" value="Sign In"/>
+                    </form>
+                </div>
             </div>
         </div>
     );
--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -5,39 +5,41 @@ import Cat from '../images/Cat.svg';
 const Register = () => {
     return (
         <div className="frontpage">
-            <div className="frontpage__title">
-                Want to get more work on top of your job?
-            </div>
-            <div className="frontpage__subtitle">
-                Use our software.
-            </div>
-            <img src={Cat} className="frontpage__img"/>
-            <div className="register__form__area">
-                <div className="register__form__title">
-                    <hr/>
-                    Register
-                    <hr/>
+            <div className="frontpage__block">
+                <div className="frontpage__title">
+                    Want to get more work on top of your job?
                 </div>
-                <form className="register__form">
-                    <div className="register__names">
-                        <label>
-                            <input type="text" name="fname" placeholder="First Name"/><br/>
-                        </label>
-                        <label>
-                            <input type="text" name="lname" placeholder="Last Name"/><br/>
-                        </label>
+                <div className="frontpage__subtitle">
+                    Use our software.
+                </div>
+                <img src={Cat} className="frontpage__img"/>
+                <div className="register__form__area">
+                    <div className="register__form__title">
+                        <hr/>
+                        Register
+                        <hr/>
                     </div>
-                    <label>
-                        <input type="text" name="email" placeholder="Email Address"/><br/>
-                    </label>
-                    <label>
-                        <input type="password" name="password" placeholder="Password"/><br/>
-                    </label>
-                    <label>
-                        <input type="cpassword" name="cpassword" placeholder="Confirm Password"/><br/>
-                    </label>
-                    <input type="submit" className="register__form__submit" value="Sign In"/>
-                </form>
+                    <form className="register__form">
+                        <div className="register__names">
+                            <label>
+                                <input type="text" name="fname" placeholder="First Name"/><br/>
+                            </label>
+                            <label>
+                                <input type="text" name="lname" placeholder="Last Name"/><br/>
+                            </label>
+                        </div>
+                        <label>
+                            <input type="text" name="email" placeholder="Email Address"/><br/>
+                        </label>
+                        <label>
+                            <input type="password" name="password" placeholder="Password"/><br/>
+                        </label>
+                        <label>
+                            <input type="cpassword" name="cpassword" placeholder="Confirm Password"/><br/>
+                        </label>
+                        <input type="submit" className="register__form__submit" value="Sign In"/>
+                    </form>
+                </div>
             </div>
         </div>
     );
