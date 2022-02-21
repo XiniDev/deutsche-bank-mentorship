eca20c638d58f5a81e25ac5e053374377ae2541d
Savisdead <Savisdead@gmail.com>
2022-02-21 20:33:30 +0000

    cont
    My contribution d

---
+++ project/user/templates/user/Login.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import Cat from '../images/Cat.svg';
+
+const Login = () => {
+    return (
+        <div className="frontpage">
+            <div className="frontpage__block">
+                <div className="frontpage__title">
+                    Want to get more work on top of your job?
+                </div>
+                <div className="frontpage__subtitle">
+                    Use our software.
+                </div>
+                <img src={Cat} className="frontpage__img"/>
+                <div className="login__form__area">
+                    <div className="login__form__title">
+                        <hr/>
+                        Sign In
+                        <hr/>
+                    </div>
+                    <form className="login__form">
+                        <label>
+                            <input type="text" name="email" placeholder="Email Address"/><br/>
+                        </label>
+                        <label>
+                            <input type="password" name="password" placeholder="Password"/><br/>
+                        </label>
+                        <input type="submit" className="login__form__submit" value="Sign In"/>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
