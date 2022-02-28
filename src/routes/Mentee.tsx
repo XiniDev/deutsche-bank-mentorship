6c3bb7d342942e8c6728d1e21ab1ac1d4a4081b3
Anthony Leung <anthonyleung07@gmail.com>
2022-02-28 01:54:39 +0000

    fixed buttons inside mentee page
    buttons are fixed yay :D
    
    also, fixed a typo inside app.tsx
    
    installed jquery inside the packages

--- src/routes/Mentee.tsx
+++ src/routes/Mentee.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Bar from './Bar';
 import snalProfile from '../images/snalProfile.png';
 import plus from '../images/plus.svg';
@@ -6,132 +6,119 @@ import FormDialog from './test';
 
 import { Link } from "react-router-dom";
 
-const Mentee = () => {
-    return (
-        <div className='background'>
-            <div className='container'>
-                <Bar />
-                <div className='content__background'>
-                    <div className='content'>
-                        <h1>Mentee</h1>
-                        <hr />
-                        <div className='mentee__box'>
-                            <img src={snalProfile} className="user__box__icon"/>
-                            <div className='user__box__info'>
-                                <h2>Snal</h2>
-                                <p>He/Him</p>
-                                <div className='user__box__title'>IT (Invertebrate Technology)</div>
-                            </div>
-                        </div>
-                        <hr />
+import $ from 'jquery';
+import { render } from '@testing-library/react';
 
-                        <div className='upcoming__sessions'>
-                            <h1>Upcoming Sessions</h1>
-                            <Link to="/addsession">
-                                <div className='add__session__button'>
-                                    <img src={plus} className="plus__symbol"/>
-                                    New Session
+class Mentee extends Component {
+    componentDidMount() {
+        $(function() {
+            $(".expand__button").on("click", function() {
+                console.log("I have been clicked");
+                $(this).parent().parent().toggleClass("active");
+                var content = $(this).parent().parent().children().last();
+                if (content.css("max-height") == "0px"){
+                    content.css("max-height", content.prop("scrollHeight")+ "px");
+                } else {
+                    content.css("max-height", "0px");
+                } 
+            });
+        });
+    }
+    render() {
+        return (
+            <div className='background'>
+                <div className='container'>
+                    <Bar />
+                    <div className='content__background'>
+                        <div className='content'>
+                            <h1>Mentee</h1>
+                            <hr />
+                            <div className='mentee__box'>
+                                <img src={snalProfile} className="user__box__icon"/>
+                                <div className='user__box__info'>
+                                    <h2>Snal</h2>
+                                    <p>He/Him</p>
+                                    <div className='user__box__title'>IT (Invertebrate Technology)</div>
                                 </div>
-                            </Link>
-                        </div>
+                            </div>
+                            <hr />
 
-                        <div className='session__box'>
-                            <div className='session__overview'>
-                                <div className='session__info'>
-                                    <div className='session__title'>
-                                        Wednesday, Feburary 2nd 
-                                        <div className='user__box__tag'>Jump</div>
+                            <div className='upcoming__sessions'>
+                                <h1>Upcoming Sessions</h1>
+                                <Link to="/addsession">
+                                    <div className='add__session__button'>
+                                        <img src={plus} className="plus__symbol"/>
+                                        New Session
                                     </div>
-                                    Focus: Jumping vertically, from flat ground
-                                </div>
-                                <img src={plus} className="expand__button" onClick={expandPlanOfAction}/>
+                                </Link>
                             </div>
-                            <div className="plan__of__action">
-                                <div className='plan__tag'>Straighten back</div>
-                                <div className='plan__tag'>Jumping position</div>
-                                <div className='plan__tag'>Make the jump</div>
-                                <div className='plan__tag'>Land without fail</div>
-                            </div>
-                        </div>
 
-                        <div className='session__box'>
-                            <div className='session__overview'>
-                                <div className='session__info'>
-                                    <div className='session__title'>
-                                        Friday, Feburary 4th 
-                                        <div className='user__box__tag'>Jump</div>
+                            <div className='session__box'>
+                                <div className='session__overview'>
+                                    <div className='session__info'>
+                                        <div className='session__title'>
+                                            Wednesday, Feburary 2nd 
+                                            <div className='user__box__tag'>Jump</div>
+                                        </div>
+                                        Focus: Jumping vertically, from flat ground
                                     </div>
-                                    Focus: Jumping through hoops
+                                    <img src={plus} className="expand__button"/>
                                 </div>
-                                <img src={plus} className="expand__button"/>
+                                <div className="plan__of__action">
+                                    <div className='plan__tag'>Straighten back</div>
+                                    <div className='plan__tag'>Jumping position</div>
+                                    <div className='plan__tag'>Make the jump</div>
+                                    <div className='plan__tag'>Land without fail</div>
+                                </div>
                             </div>
-                            <div className="plan__of__action">
-                                <div className='plan__tag'>Straighten back</div>
-                                <div className='plan__tag'>Jumping position</div>
-                                <div className='plan__tag'>Make the jump</div>
-                                <div className='plan__tag'>Land without fail</div>
-                            </div>
-                        </div>
 
-                        <hr/>
-                        <h1>Previous Sessions</h1>
-
-                        <div className='session__box'>
-                            <div className='session__overview'>
-                                <div className='session__info'>
-                                    <div className='session__title'>
-                                        Friday, January 28th 
-                                        <div className='user__box__tag'>Jump</div>
+                            <div className='session__box'>
+                                <div className='session__overview'>
+                                    <div className='session__info'>
+                                        <div className='session__title'>
+                                            Friday, Feburary 4th 
+                                            <div className='user__box__tag'>Jump</div>
+                                        </div>
+                                        Focus: Jumping through hoops
                                     </div>
-                                    Focus: Learning what jumping is
+                                    <img src={plus} className="expand__button"/>
                                 </div>
-                                <img src={plus} className="expand__button"/>
+                                <div className="plan__of__action">
+                                    <div className='plan__tag'>Straighten back</div>
+                                    <div className='plan__tag'>Jumping position</div>
+                                    <div className='plan__tag'>Make the jump</div>
+                                    <div className='plan__tag'>Land without fail</div>
+                                </div>
                             </div>
-                            <div className="plan__of__action">
-                                <div className='plan__tag'>Straighten back</div>
-                                <div className='plan__tag'>Jumping position</div>
-                                <div className='plan__tag'>Make the jump</div>
-                                <div className='plan__tag'>Land without fail</div>
+
+                            <hr/>
+                            <h1>Previous Sessions</h1>
+
+                            <div className='session__box'>
+                                <div className='session__overview'>
+                                    <div className='session__info'>
+                                        <div className='session__title'>
+                                            Friday, January 28th 
+                                            <div className='user__box__tag'>Jump</div>
+                                        </div>
+                                        Focus: Learning what jumping is
+                                    </div>
+                                    <img src={plus} className="expand__button"/>
+                                </div>
+                                <div className="plan__of__action">
+                                    <div className='plan__tag'>Straighten back</div>
+                                    <div className='plan__tag'>Jumping position</div>
+                                    <div className='plan__tag'>Make the jump</div>
+                                    <div className='plan__tag'>Land without fail</div>
+                                </div>
                             </div>
+                            <hr />
                         </div>
-                        <hr />
                     </div>
                 </div>
             </div>
-        </div>
-    );
-}
-
-//lol just do this haha:
-
-function expandPlanOfAction() {
-    var hi = document.getElementsByClassName("expand__button");
-    hi[0].addEventListener("click", function(this:any) {
-        console.log("I have been clicked");
-        this.parentElement.parentElement.classList.toggle("active");
-        var content = this.parentElement.parentElement.lastChild;
-        if (content.style.maxHeight){
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        } 
-    });
-}
-
-var coll = document.getElementsByClassName("expand__button");
-var i;
-for (i = 0; i < coll.length; i++) {
-    console.log("Adding event listener for: " + i);
-    coll[i].addEventListener("click", function(this:any) {
-        console.log("I have been clicked");
-        this.parentElement.parentElement.classList.toggle("active");
-        var content = this.parentElement.parentElement.lastChild;
-        if (content.style.maxHeight){
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        } 
-    });
+        );
+    }
 }
 
 var plans = document.getElementsByClassName("plan__tag");
