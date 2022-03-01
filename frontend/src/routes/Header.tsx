e86e521b6a27d5d44732b90ecd74b44595f893b8
Savisdead <Savisdead@gmail.com>
2022-03-01 00:04:11 +0000

    login function
    pushing this shit

--- frontend/src/routes/Header.tsx
+++ frontend/src/routes/Header.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-
+//import { useCookies, withCookies, Cookies } from 'react-cookie';
 import Rattus1 from '../images/Rat Logo.svg';
+import { Console } from 'console';
+import { useCookies } from 'react-cookie';
 
 const Header = () => {
+
+    const [token, setToken,removeToken] = useCookies(['mytoken'])
+    //const myCookie = cookies.get('myCookie');
+    //console.log(myCookie);
+
+    const logoutBtn = () => {
+       // cookies.remove('token', { path: '/' });
+        setToken('mytoken', null, { path: '/' });
+
+    }
+    
+    
     return (
         <header className="header">
             <div className="header__logo">
@@ -29,6 +43,10 @@ const Header = () => {
                         Profile
                     </div>
                 </Link>
+
+                <div className="header__signinup" onClick = {logoutBtn}>
+                    Sign out
+                </div>
                 
             </div>
         </header>
--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -1,8 +1,46 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
+import APIService from '../APIService';
+import {useCookies} from 'react-cookie';
+import {useNavigate} from 'react-router-dom';
 import Cat from '../images/Cat.svg';
 
+
 const Login = () => {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [token, setToken,removeToken] = useCookies(['mytoken'])
+    const [isLogin, setLogin] = useState(true)
+    let navigate = useNavigate()
+
+    
+    removeToken('mytoken');
+
+    const loginBtn = () => {
+        APIService.LoginUser({username, password})
+        .then(resp => setToken('mytoken',resp.token))
+        .catch(error => console.log(error))
+        
+        navigate('/profile')
+       
+        
+    }
+    
+    const RegisterBtn = () => {
+        
+        APIService.RegisterUser({username, password})
+        .then( () => loginBtn())
+        .catch( error => console.log(error))
+    }
+
+    useEffect(() => {
+        
+        if(token['mytoken']) {
+            navigate('/profile')
+
+        }
+    }, [token])
+
     return (
         <div className="frontpage">
             <div className="frontpage__block">
@@ -21,12 +59,14 @@ const Login = () => {
                     </div>
                     <form className="login__form">
                         <label>
-                            <input type="text" name="email" placeholder="Email Address"/><br/>
+                            <input type="text" name="username" id="username" placeholder="Username"
+                            value = {username} onChange = {e => setUsername(e.target.value)}/><br/>
                         </label>
                         <label>
-                            <input type="password" name="password" placeholder="Password"/><br/>
+                            <input type="password" name="password" id="password" placeholder="Password"
+                            value = {password} onChange = {e => setPassword(e.target.value)}/><br/>
                         </label>
-                        <input type="submit" className="submit__button" value="Sign In"/>
+                        <input type="submit" className="submit__button" value="Sign In" onClick = {loginBtn}/>
                     </form>
                 </div>
             </div>
@@ -35,3 +75,4 @@ const Login = () => {
 }
 
 export default Login;
+
