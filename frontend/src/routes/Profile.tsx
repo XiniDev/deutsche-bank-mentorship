a997f782555c70eb70fc428b284a4274942378f0
Anthony Leung <anthonyleung07@gmail.com>
2022-03-12 05:36:27 +0000

    fixed profile
    made profile way cleaner now, like the method to display stuff is much cleaner

--- frontend/src/routes/Profile.tsx
+++ frontend/src/routes/Profile.tsx
@@ -7,59 +7,36 @@ import {useCookies} from 'react-cookie';
 import { Link } from "react-router-dom";
 import APIService from '../APIService';
 
-function RenderGroup(group:any){
-        
-    if(group.length == 1){
-        return(
-            <div className='profile__specialisations'>
-                <div className='tag__wrapper'>
-                    <div className='specialisation__tag'>{group[0].topic}</div>
-                        <p>{group[0].description}</p>
-                </div>
-            </div>
-        )
-    }else if(group.length == 2){
-        return(
-            <div className='profile__specialisations'>
-                <div className='tag__wrapper'>
-                    <div className='specialisation__tag'>{group[0].topic}</div>
-                        <p>{group[0].description}</p>
-                </div>
-                <div className='tag__wrapper'>
-                    <div className='specialisation__tag'>{group[1].topic}</div>
-                        <p>{group[1].description}</p>
-                </div>
-            </div>
-        )
-        
-    }else if(group.length == 3){
-        return(
-            <div className='profile__specialisations'>
-                <div className='tag__wrapper'>
-                    <div className='specialisation__tag'>{group[0].topic}</div>
-                        <p>{group[0].description}</p>
-                </div>
-                <div className='tag__wrapper'>
-                    <div className='specialisation__tag'>{group[1].topic}</div>
-                        <p>{group[1].description}</p>
-                </div>
-                <div className='tag__wrapper'>
-                    <div className='specialisation__tag'>{group[2].topic}</div>
-                        <p>{group[2].description}</p>
-                </div>
-            </div>
-        )
-    }
+const ShowSpecs = (spec:any) => {
+    return (
+        <div className='tag__wrapper'>
+            <div className='specialisation__tag'>{spec.spec.topic}</div>
+                <p>{spec.spec.description}</p>
+        </div>
+    )
+}
+
+const ShowIntrs = (intr:any) => {
+    return (
+        <div className='tag__wrapper'>
+            <div className='interest__tag'>{intr.intr.topic}</div>
+                <p>{intr.intr.description}</p>
+        </div>
+    )
+}
+
+const ShowFail = () => {
     return (<div className='tag__wrapper'>Click edit to add some entries!</div>)
 }
+
 const Profile = () => {
+    const [token] = useCookies(['mytoken'])
 
     const [userID, setID] = useState<any>([])
     const [userDetails, setDetails] = useState<any>([])
     const [profile, setProfile] = useState<any>([])
-    const [specs, setSpecs] = useState<any>([])
-    const [interests, setInterests] = useState<any>([])
-    const [token] = useCookies(['mytoken'])
+    const [specs, setSpecs] = useState<any[]>([])
+    const [intrs, setIntrs] = useState<any[]>([])
 
     useEffect(() => {
         
@@ -67,10 +44,12 @@ const Profile = () => {
         APIService.getProfile(userID, token['mytoken']).then(resp => setProfile(resp))
         APIService.getUserDetails(userID, token['mytoken']).then(resp => setDetails(resp))
         APIService.getSpecialties(token['mytoken']).then(resp => setSpecs(APIService.GetGroupByID(userID,resp,"userID")))
-        APIService.getInterests(token['mytoken']).then(resp => setInterests(APIService.GetGroupByID(userID,resp,"userID")))
+        APIService.getInterests(token['mytoken']).then(resp => setIntrs(APIService.GetGroupByID(userID,resp,"userID")))
         
-        
-    }, [userID])   
+    }, [userID])
+
+    const specsList = specs ? specs.map((spec) => <ShowSpecs key={spec.topic} spec={spec}/>) : <ShowFail/>
+    const intrsList = intrs ? intrs.map((intr) => <ShowIntrs key={intr.topic} intr={intr}/>) : <ShowFail/>
 
     return (
         <div className='background'>
@@ -106,7 +85,9 @@ const Profile = () => {
                                 Edit
                             </Link>
                         </div>
-                        {RenderGroup(specs)}
+                        <div className='profile__specialisations'>
+                            {specsList}
+                        </div>
                         <hr />
 
                         <div className="profile__headers">
@@ -116,7 +97,9 @@ const Profile = () => {
                                 Edit
                             </Link>
                         </div>
-                        {RenderGroup(interests)}
+                        <div className='profile__interests'>
+                            {intrsList}
+                        </div>
                         <hr />
                     </div>
                 </div>
