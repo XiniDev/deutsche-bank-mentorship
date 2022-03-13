fb5c96716bcd8eaa19407c728b0cf100a1d3f4cd
Katie Yang <67641765+KatietheMushroom@users.noreply.github.com>
2022-03-13 15:52:27 +0000

    Group session thingy

--- frontend/src/routes/AddGroupSession.tsx
+++ frontend/src/routes/AddGroupSession.tsx
@@ -1,13 +1,54 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useState, useEffect, CSSProperties } from 'react';
 import Bar from './Bar';
-import Select from 'react-select'
-import { Link } from 'react-router-dom';
-
+import Select, { StylesConfig } from 'react-select'
+import { Link, useNavigate } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+import APIService from '../APIService';
 import Alert from '@mui/material/Alert';
 import $ from 'jquery';
 
+import moment from 'moment';
 
-const options = [
+
+type OptionType = {
+    label: string;
+    value: string;
+}
+
+const customControlStyles: CSSProperties = {
+    width: '150px',
+    borderColor: "#000000",
+    color: "#000000",
+    marginTop: "10px",
+    marginLeft: "10px",
+    marginBottom: "10px",
+}
+
+const customMenuStyles: CSSProperties = {
+    width: '150px',
+    color: '#000000',
+}
+
+type IsMulti = false
+
+const selectStyle: StylesConfig<OptionType, IsMulti> = {
+    control: (provided, state) => {
+        return {
+          ...provided,
+          ...customControlStyles
+        }
+    },
+
+    menu: (provided, state) => {
+        return {
+            ...provided,
+            ...customMenuStyles
+        }
+    },
+}
+
+
+const menteeOptions = [
     { value: 'snal', label: 'Snal' },
     { value: 'bea', label: 'Bea' },
   ]
@@ -22,6 +63,69 @@ const AddGroupSession: FC = () => {
             });
         });
     }, []);
+
+    const [token] = useCookies(['mytoken'])
+    let navigate = useNavigate()
+
+    const [userID, setID] = useState<any>([])
+    const [specs, setSpecs] = useState<any>([])
+    const [options, setOptions] = useState<OptionType[]>([])
+
+    let menteeID = document.location.search.substring(1).split("=")[1]
+    const [menteeProfile, setMenteeProfile] = useState<any>([])
+
+    const [date, setDate] = useState('')
+    const [topic, setTopic] = useState('')
+    const [description, setDescription] = useState('')
+    const [location, setLocation] = useState('')
+    const [startTimeVal, setStartTimeVal] = useState<any>([])
+    const [endTimeVal, setEndTimeVal] = useState<any>([])
+
+
+    useEffect(() => {
+        APIService.getUserID(`${token['mytoken']}`,token['mytoken']).then(resp => setID(resp.user))
+        APIService.getSpecialties(token['mytoken']).then(resp => setSpecs(APIService.GetGroupByID(userID,resp,"userID"))).then(() => {
+            for (let i = 0; i < specs.length; i++) {
+                setOptions(state => [...state, {value : specs[i].topic, label : specs[i].topic}])
+            }
+        })
+    }, [userID])
+    
+    const CreateSession = async (e: React.FormEvent<HTMLInputElement>) => {
+
+        e.preventDefault()
+
+        var conditions = 0
+
+        var divsToShow = document.getElementsByClassName('error__message') as HTMLCollectionOf<HTMLElement>
+        for(var i = 0; i < divsToShow.length; i++){
+            divsToShow[i].style.display = "none"
+
+        }
+        
+        if (!date || !startTimeVal || !endTimeVal || !topic || !description || !location) {
+            conditions += 1
+            console.log("PLEASE FILL IN ALL DETAILS")
+            divsToShow[0].style.display = "block"
+        }
+        if (conditions == 0) {
+            let eventID = 0
+            let mentorID = userID
+            const start_time = moment(date + ' ' + startTimeVal).format("yyyy-MM-DD[T]HH:mm:SS[Z]")
+            const end_time = moment(date + ' ' + endTimeVal).format("yyyy-MM-DD[T]HH:mm:SS[Z]")
+            console.log({mentorID, topic, description, start_time, end_time, location})
+            APIService.SetEvents({mentorID, topic, description, start_time, end_time, location}, token['mytoken']).then(resp => {
+                eventID = resp.id
+                APIService.SetAttendees({eventID, menteeID}, token['mytoken']).then(() => {
+                    navigate("/mentee?menteeID=1")
+                    console.log()
+                })
+                .catch( error2 => console.log(error2))
+                console.log()
+            })
+            .catch( error => console.log(error))
+        }
+    }
 
     return (
         <div className='background'>
@@ -42,24 +146,41 @@ const AddGroupSession: FC = () => {
                             </div>
                             <div className='new__session__form__container'>
                                 <div className='new__session__labels'>
-                                    <p>Date</p>
-                                    <p>Time</p>
-                                    <p>Topic</p>
-                                    <p>Mentees</p>
+                                    <p>Date *</p>
+                                    <p>Time *</p>
+                                    <p>Topic *</p>
+                                    <p>Mentees *</p>
+                                    <p>Description *</p>
+                                    <p>Location *</p>
                                 </div>
-                                <form className="new__session__form">
-                                    <input type="date" name="date"/><br/>
-                                    <input type="time" name="starttime"/>
+                                <form className="new__session__form" id="new__session">
+                                    <input type="date" name="date" value = {date} onChange = {e => setDate(e.target.value)}/><br/>
+                                    <input type="time" name="starttime" value = {startTimeVal} onChange = {e => setStartTimeVal(e.target.value)}/>
                                     to
-                                    <input type="time" name="endtime"/><br/>
-                                    <select name="topic">
+                                    <input type="time" name="endtime" value = {endTimeVal} onChange = {e => setEndTimeVal(e.target.value)}/><br/>
+                                    <Select
+                                        styles={selectStyle}
+                                        defaultValue={null}
+                                        onChange={e =>{if (e) setTopic(e.value)}}
+                                        options={options}
+                                    />
+                                    <Select 
+                                        isMulti 
+                                        defaultValue={null}
+                                        options={menteeOptions} 
+                                        className='multiselect'
+                                    />
+                                    <input type="text" name="description" value = {description} onChange = {e => setDescription(e.target.value)}/><br/>
+                                    <input type="text" name="location" value = {location} onChange = {e => setLocation(e.target.value)}/><br/>
+                                    {/* <select name="topic">
+                                        <option label=" "></option>
                                         <option value="1">Jump</option>
                                         <option value="2">Cheese and Wine</option>
-                                    </select><br/>
-                                    <Select isMulti options={options} className='multiselect'/>
+                                    </select><br/> */}
+                                    <div className='error__message' id='err0'>Please fill in all fields!</div>
+                                    <input type="submit" className="submit__button" value="Add Session" onClick = {CreateSession}/>
                                 </form>
                             </div>
-                            <div className='submit__button'>Submit Feedback</div>
                         </div>
                         <hr />
                     </div>
